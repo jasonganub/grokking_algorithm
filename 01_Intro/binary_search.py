66ecfabc7b215a549d350ab09bc71ffe23f799ac
Jason Ganub <jasonganub@gmail.com>
2017-08-20 22:27:54 -0700

    Created readme

--- 01_Intro/binary_search.py
+++ 01_Intro/binary_search.py
@@ -16,12 +16,6 @@ def binary_search(list, x):
     return None
 
 
-
 if __name__ == '__main__':
     list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     print(binary_search(list, 8))
-
-
-
-
-
---
+++ README.md
@@ -0,0 +1,3 @@
+# Grokking Algorithm
+
+Solving algorithms and exercises. What better way than to implement them and version control them for memory's sake.
